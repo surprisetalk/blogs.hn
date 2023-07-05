700bc0c0ca02545ddeb3fb764d07d3249aad6db1
Taylor Troesh <surprisetalk@gmail.com>
2023-07-05 08:28:47 -0700

    😞

--- build.js
+++ build.js
@@ -45,4 +45,26 @@ for (const blog of blogs) {
   index += `</div>`;
 }
 
-fs.writeFileSync("./dist/index.html", template.replace("{{main}}", index));
+fs.writeFileSync(
+  "./dist/index.html",
+  template.replace("{{body}}", `<main id="index">${index}</main>`)
+);
+
+const about = `
+  <div>
+    <h2>hello world,</h2>
+    <p><a href="/">blogs.hn</a> is a directory of tech sites, primarily sourced from <a href="https://news.ycombinator.com">HackerNews</a>.</p>
+    <p>To submit/update a blog, <a href="https://github.com/surprisetalk/blogs.hn/blob/main/blogs.json">edit blogs.json in a pull-request</a>.</p>
+    <p>If you like small sites with RSS feeds, consider checking out <a href="https://ooh.directory">ooh.directory</a>. If you don't have an RSS reader, I highly recommend <a href="https://reederapp.com">Reeder 5</a>.</p>
+    <p>And if you aren't already, <em>you should</em> <a href="https://www.benkuhn.net/writing/">write things on the internet</a>. All you need is <a href="https://github.com/surprisetalk/worstpress">worstpress</a> and some markdown files! You can <a href="mailto:hello@taylor.town">email me</a> if you need help getting started.</p>
+    <p>Every blog is a window into a skull. Don't be afraid to ask questions and kindle friendships! Remember to be kind, courteous, and succinct.</p>
+    <p>Thanks for stopping by.</p>
+    <br/>
+    <p style="padding-left: 1rem;">🐸 <a href="https://taylor.town"><strong>Taylor Troesh</strong></a></p>
+  </div>
+`;
+
+fs.writeFileSync(
+  "./dist/about.html",
+  template.replace("{{body}}", `<main id="about">${about}</main>`)
+);
